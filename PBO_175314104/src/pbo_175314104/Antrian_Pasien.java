26763ffb85f7944d5403b051d9428fadac706d45
Yeski's Legion <Yeski's Legion@LAPTOP-T5591HGH.mshome.net>
2018-09-05 01:05:54 +0700

    praktikum

---
+++ PBO_175314104/src/pbo_175314104/Antrian_Pasien.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_175314104;
+
+public class Antrian_Pasien {
+ private int tanggalantrian,bulanantrian, tahunantrian; //variabel integer bersifat private
+ private Klinik klinik; //variabel Klinik bersifat private
+    private Pasien daftarPasien[]; //array bertipe data Pasien bersifat private
+
+    public void setKlinik(Klinik klinik) { //method void setKlinik dengan parameter klinik
+        this.klinik = klinik;
+    }
+    
+     public Klinik getKlinik() {//method getKlinik yang mereturn variabel bertipe data klinik
+        return klinik;
+    }
+     
+    public void setDaftarPasien(Pasien[] daftarPasien) { //method void setDaftarPasien parameter array daftarPasien bertipe data Pasien
+        this.daftarPasien = daftarPasien;
+    }
+
+    public Pasien[] getDaftarPasien() { //method getDaftarPasien yang mereturn array data Pasien
+        return daftarPasien;
+    }
+
+   
+ 
+}
--- PBO_175314104/src/pbo_175314104/Dokter.java
+++ PBO_175314104/src/pbo_175314104/Dokter.java
@@ -5,10 +5,53 @@
  */
 package pbo_175314104;
 
-/**
- *
- * @author admin
- */
 public class Dokter {
+    private String nomorDokter,nama,alamat,tempatLahir; //variabel String dan bersifat private
+    private int tanggalLahir; //variabel integer dan bersifat private
     
+    public void setNomorDokter(String nomorDokter) throws NumberFormatException{ //method void dengan throws NumberFormatException
+        if (nomorDokter.toCharArray().length>=6) //Method ini mengalokasikan array karakter baru
+        this.nomorDokter = nomorDokter;
+        else{
+            throw new NumberFormatException("Nomor Dokter Salah"); //NumberFormatException berfungsi untuk menangani kesalahan dalam format penulisan angka.
+    }
+    }
+    
+    public String getNomorDokter() {
+        return nomorDokter;
+    }
+     
+    public void setNama(String nama) { //method void setNama dengan parameter variabel nama String
+        this.nama = nama;
+    }
+        
+    public String getNama() { //method getNama yang akan mereturn variabel String
+        return nama;
+    }
+
+    public void setAlamat(String alamat) {  //method void setAlamat dengan parameter variabel alamat String
+        this.alamat = alamat;
+    }
+    
+    public String getAlamat() { //method getAlamat yang mereturn variabel String
+        return alamat;
+    }
+     
+    public void setTempatLahir(String tempatLahir) {  //method void setTempatLahir dengan parameter variabel tempatLahir bertipe integer
+        this.tempatLahir = tempatLahir;
+    }
+    
+    public String getTempatLahir() { //method getTempatLahir yang akan mereturn variabel String
+        return tempatLahir;
+    }
+   
+    public void setTanggalLahir(int tanggalLahir) throws NumberFormatException{ //method void setTanggalLahir dengan parameter variabel tanggalLahir bertipe integer
+        if(tanggalLahir>0 && tanggalLahir<32)
+        this.tanggalLahir = tanggalLahir;
+        else throw new NumberFormatException("Tanggal Lahir Tidak Valid");
+    }  
+    
+    public int getTanggalLahir() {   //method getTanggalLahir yang mereturn variabel integer
+        return tanggalLahir;
+    }
 }
